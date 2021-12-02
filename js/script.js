7987691a01518cfb9a475e0eec9bbae159840610
Anderey Prokhorov <beta737@yandex.ru>
2021-12-02 15:57:00 +0300

    Выполнил обязательное задание 6 урока

--- js/script.js
+++ js/script.js
@@ -1,1 +1,27 @@
+"use strict";
 
+
+
+function playBot() {
+    function gameIteration() {
+        const random = 10;
+        let value = prompt('Число от 1 до 100')
+        value = typeof value === 'string' ? Number(value) : null;
+        if (value === random) {
+            alert("Поздравляю, Вы угадали!!!");
+        } else if (value === null) {
+            alert("Игра окончена");
+        } else if (random < value) {
+            alert("Загаданное число меньше. Введите новое число.");
+            gameIteration();
+        } else if (random > value) {
+            alert("Загаданное число больше. Введите новое число.");
+            gameIteration();
+        } else if (typeof value !== Number) {
+            alert("Введи число!");
+            gameIteration();
+        }
+    }
+    gameIteration();
+}
+playBot();
